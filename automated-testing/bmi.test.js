3633f719eac617c0b79f3e1e780c3b680857d4a6
Iven <florivenc@yahoo.com>
2022-11-17 08:48:50 +0800

    Added File

--- automated-testing/bmi.test.js
+++ automated-testing/bmi.test.js
@@ -7,3 +7,14 @@ test("Check Body Mass Index of 1.75 Meters and 75 Kg", () =>{
 test("Check Body Mass Index of 1.52 Meters and 60 Kg", () =>{
     expect(myFunction.getBMI(1.52,60)).toBeCloseTo(25.97);
 });
+
+//Test Case for Failed---------
+
+// test("Check Body Mass Index of 1.75 Meters and 75 Kg", () =>{
+//     expect(myFunction.getBMI(1.75,75)).toBeCloseTo(24.50);
+// });
+
+// test("Check Body Mass Index of 1.52 Meters and 60 Kg", () =>{
+//     expect(myFunction.getBMI(1.52,60)).toBeCloseTo(25.62);
+// });
+
